a3620bef02538bae8f07a9f95d50d167f0876852
Lumiere <lumieremondo@gmail.com>
2021-05-15 00:04:10 +0200

    Navigation Bar

---
+++ react-master/src/App/App.js
@@ -0,0 +1,13 @@
+
+import '../App.css';
+import React from "react";
+import AppBar from '../AppBar';
+function App() {
+  return (
+    <div className="App">
+        <AppBar/>
+    </div>
+  );
+}
+
+export default App;
